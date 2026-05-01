309270c3d45eb16d2f09ce703b9f44287a5f3212
FioPinto <fiorelha.pinto@alantra.com>
2026-05-01 19:47:13 +0200

    creacion de dimensiones

---
+++ models/marts/dim_estaciones.sql
@@ -0,0 +1,16 @@
+{{ config(
+    materialized='incremental',
+    unique_key='id_estacion',
+    incremental_strategy='merge'
+) }}
+
+select
+    id_estacion,
+    nombre_estacion,
+    municipio,
+    provincia,
+    current_timestamp() as updated_at
+from {{ ref('int_estaciones_limpio') }}
+
+{% if is_incremental() %}
+{% endif %}
